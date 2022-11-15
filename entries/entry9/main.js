d746ad5f7b19a41a2ad24e2aa2e9010579befd53
zhouy526 <zhouy526@newschool.edu>
2022-11-15 11:40:55 -0500

    e9

--- entries/entry9/main.js
+++ entries/entry9/main.js
@@ -4,19 +4,20 @@ let container = document.querySelector(".wrapper");
 
 
 
-  // place our blobs randomly
-  blobs.forEach(function (blob) {
-    blob.style.right = (80 * Math.random()) + '%';
-    blob.style.top = (50 * Math.random()) + '%';
-  });
-
   function turnNight() {
   body.classList.toggle("bodyNight");
   blob.classList.toggle("blobNight");
 }
 
-
 body.addEventListener("click", turnNight);
+
+// place our blobs randomly
+  blobs.forEach(function (blob) {
+    blob.style.right = (80 * Math.random()) + '%';
+    blob.style.top = (50 * Math.random()) + '%';
+  });
+
+body.addEventListener("click", blob);
 
   // container.addEventListener("click", function (event) {
   //   if (event.target.classList.contains("blob")) {
