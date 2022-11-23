a6a4eb2c3d16a8f769773dd9428b3d7e0d710220
zhouy526 <zhouy526@newschool.edu>
2022-11-22 23:26:25 -0500

    e10

---
+++ entries/entry10/main.js
@@ -0,0 +1,45 @@
+let body = document.body;
+let blobs = document.querySelectorAll(".blob");
+let container = document.querySelector(".wrapper");
+
+
+
+  function turnNight() {
+  body.classList.toggle("bodyNight");
+  blob.classList.toggle("blobNight");
+}
+
+body.addEventListener("click", turnNight);
+
+// place our blobs randomly
+  blobs.forEach(function (blob) {
+    blob.style.right = (100 * Math.random()) + '%';
+    blob.style.top = (150 * Math.random()) + '%';
+  });
+
+body.addEventListener("click", blob);
+
+  // container.addEventListener("click", function (event) {
+  //   if (event.target.classList.contains("blob")) {
+  //     event.target.remove();
+  //   }
+  // });
+
+
+
+// let body = document.body;
+// let box = document.querySelector(".box");
+// let bulb = document.querySelector(".bulb");
+
+
+// function turnOnLight() {
+//   body.classList.toggle("light");
+//   button.classList.toggle("buttonOn");
+//   box.classList.toggle("boxActive");
+//   bulb.classList.toggle("bulbOn");
+// }
+
+
+
+// let button = document.querySelector(".button");
+// button.addEventListener("click", turnOnLight);
